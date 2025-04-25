5eeeac3341e05c028f51b496f267f43aa5307a6b
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-04-25 20:41:50 +0530

    Create Swaping by 3 variable.c

---
+++ LAB/Swaping by 3 variable.c
@@ -0,0 +1,21 @@
+//WAP to Swap contains of variable A and B
+//With using extra variable C
+
+#include <stdio.h>
+int main()
+{
+	printf("Harsh Vijay Vedpathak\n24030331378026 \nVLSI Batch-B\n");
+	
+	int a,b,c;
+	printf("Enter the Variable A :");
+	scanf("%d", &a);
+	printf("Enter the Variable B :");
+	scanf("%d", &b);
+	printf("Before Swapping A = %d and B =%d \v",a,b);
+	c=a;
+	a=b;
+	b=c;
+	printf("After Swapping A = %d and B =%d",a,b);
+	
+	return 0;
+}
