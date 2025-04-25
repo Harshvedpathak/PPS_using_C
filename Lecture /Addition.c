5e64f93701fe490a3533c87b98cee7615bed15d3
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-04-25 20:25:20 +0530

    Create Addition.c

---
+++ Lecture /Addition.c
@@ -0,0 +1,13 @@
+// Addition of two numbers input through keyboard
+#include<stdio.h>
+int main()
+{
+  int num1,num2,sum;
+  printf("Enter First Number: ");
+  scanf("%d", &num1);
+  printf("Enter Second Number: ");
+  scanf("%d", &num2);
+  sum=num1+num2;
+  printf("The Addition of %d and %d is %d",num1,num2,sum);
+  return 0;
+}
