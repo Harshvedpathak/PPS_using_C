d887a8343d24dea3e5e276a5519b9ad894b87893
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-04-29 21:44:02 +0530

    Create Grades.c

---
+++ LAB/Grades.c
@@ -0,0 +1,41 @@
+//Harsh Vedpathak 8026
+#include <stdio.h>
+int main()
+{
+    int per;
+    printf("Enter the obtained Percentage:");
+    scanf("%d",&per);
+    if(per>100||per<0)
+    {
+    	printf("invalid marks, Enter valid Percentage obtained");
+	}
+	else if(per>=90)
+    {
+    	printf(" grade obtained is Excellent");
+	}
+	else if(per>=80)
+    {
+    	printf(" grade obtained is A");
+	}
+	else if(per>=70)
+    {
+    	printf(" grade obtained is B");
+	}
+	else if(per>=60)
+    {
+    	printf(" grade obtained is C");
+	}
+	else if(per>=50)
+    {
+    	printf(" grade obtained is D");
+	}
+	else if(per>=40)
+    {
+    	printf(" grade obtained is E");
+	}
+	else 
+	{
+    	printf(" grade obtained is FAIL");
+	}
+	return 0;
+}
