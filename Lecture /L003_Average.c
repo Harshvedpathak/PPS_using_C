1a2ceae3d0100f2bd03a30277e989db8835ebda1
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-05-01 15:44:26 +0530

    Update L003_Average.c

--- Lecture /L003_Average.c
+++ Lecture /L003_Average.c
@@ -2,9 +2,9 @@
 int main() 
 {
     int num1,num2,avg;
-    printf("Enter first number");
+    printf("Enter first number : ");
     scanf("%d", &num1);
-    printf("Enter second number");
+    printf("Enter second number : ");
     scanf("%d", &num2);
     avg=(num1+num2)/2;
     printf("The average of %d and %d is %d", num1, num2, avg);
