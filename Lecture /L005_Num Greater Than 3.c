181523669e7c0defa03e4cdeff09e8f9167c28a9
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-05-05 16:49:10 +0530

    Create L005_Num Greater Than 3.c
    Example of if statement

---
+++ Lecture /L005_Num Greater Than 3.c
@@ -0,0 +1,17 @@
+//Lecture 15/04/2025
+// 'if' Statement
+//WAP to Print "yes" if the given number is greater than 3
+
+#include <stdio.h>
+int main()
+{
+  int num;
+  printf("Enter the number: ");
+  scanf("%d",&num);
+  if(num>3)
+  {
+    printf("Yes");
+  }
+  printf("End of Program");
+  return 0;
+}
