3adfe351b8198327ec3c6efa568bc25ba5d63aaa
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-05-08 12:43:52 +0530

    Update L010_Print 1 to 10 numbers.c

---
+++ Lecture /L010_Print 1 to 10 numbers.c
@@ -0,0 +1,13 @@
+//24-04-2025
+//Lecture_Example of For Loop.        
+//WAP to print numbers from 1 to 10
+
+#include <stdio.h>
+int main()
+{
+  for(int i=1;i<11;i++)
+    {
+      printf("%d\n",i);
+    }
+  return 0;  
+}
