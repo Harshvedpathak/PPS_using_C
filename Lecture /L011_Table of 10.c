77cd4a7dfd1257bbf2493c466a0fdf18c8156749
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-05-08 13:45:07 +0530

    Update L011_Table of 10.c

---
+++ Lecture /L011_Table of 10.c
@@ -0,0 +1,13 @@
+//24-04-2025
+//Lecture_WAP to print table of 10
+// Example of For Loop
+
+#include <stdio.h>
+int main()
+{int i;
+  for (i = 1; i <= 10; i++) 
+  {
+    printf("10 X %d = %d\n", i, i*10);
+  }
+  return 0;
+}
