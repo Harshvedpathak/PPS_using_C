8c6b19a3c00ec92ccf900b98bfd775da1ef7d61d
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-05-08 15:49:19 +0530

    Update L012_Even and Odd number between 0 and 10.c

--- Lecture /L012_Even and Odd number between 0 and 10.c
+++ Lecture /L012_Even and Odd number between 0 and 10.c
@@ -1,1 +1,19 @@
+//24-04-2025
+//Lecture_WAP to print all the Even numbers between 0 & 10.
 
+#include <stdio.h>
+int main()
+{
+  int i;
+  printf("The Even Numbers between 0 to 10 Are :\n");
+  for (i=0;i<=5;i++)
+    {
+      printf("%d\n",i*2);
+    }
+  printf("\nThe Odd Numbers between 0 to 10 Are :\n");
+  for (i=0;i<=4;i++)
+    {
+      printf("%d\n",i*2+1);
+    }
+  return 0;
+}
