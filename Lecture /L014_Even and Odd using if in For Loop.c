581ab22dd98ff0352d75456f9981534c8add89c4
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-05-10 16:47:44 +0530

    Create L014_Even and Odd using if in For Loop.c

---
+++ Lecture /L014_Even and Odd using if in For Loop.c
@@ -0,0 +1,22 @@
+//24-04-2025
+//Lecture/HW_WAP to print all the Even numbers between 0 & 10.
+//Using if condition in For Loop
+
+#include <stdio.h>
+int main()
+{
+  int i;
+  printf("The Even Numbers between 0 to 10 Are :\n");
+  for (i=0;i<=10;i++)
+    {
+      if(i % 2==0)
+      printf("%d\n",i);
+    }
+  printf("\nThe Odd Numbers between 0 to 10 Are :\n");
+  for (i=0;i<=10;i++)
+    {
+      if(i % 2 != 0)
+      printf("%d\n",i);
+    }
+  return 0;
+}
