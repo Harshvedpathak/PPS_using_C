ee80d024158a11bf26df9a7ab7d8e0b6d37fa25e
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-05-20 00:01:39 +0530

    Create Factorial.c

---
+++ LAB/Factorial.c
@@ -0,0 +1,16 @@
+//LAB_15/05/25
+//WAP to get factorial of input number
+
+#include <stdio.h>
+int main()
+{
+  int f;
+  printf("Enter the number to get its Factotial: ");
+  scanf("%d",f);
+  for (i=1;i<=f;i++)
+    {
+      fact *=i;
+    }
+  printf("The Factorial of %d is %d",f,fact);
+}
+return 0;
