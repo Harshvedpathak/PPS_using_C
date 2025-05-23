7d19cf7bc57ee126674bf36442f1394ec7b143df
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-05-24 00:51:56 +0530

    Update and rename L014_Even and Odd using if in For Loop.c to L014_Print Even number using if in For Loop.c

--- Lecture /L014_Print Even number using if in For Loop.c
+++ Lecture /L014_Print Even number using if in For Loop.c
@@ -12,11 +12,5 @@ int main()
       if(i % 2==0)
       printf("%d\n",i);
     }
-  printf("\nThe Odd Numbers between 0 to 10 Are :\n");
-  for (i=0;i<=10;i++)
-    {
-      if(i % 2 != 0)
-      printf("%d\n",i);
-    }
   return 0;
 }
