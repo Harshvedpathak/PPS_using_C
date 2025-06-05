4c6ab6234c19c6b1f3ae89e455ad75cfcd34c07a
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-06-05 10:26:31 +0530

    Create Patern Printing  using for Loop.c

---
+++ LAB/Patern Printing  using for Loop.c
@@ -0,0 +1,18 @@
+// PPS Lab
+// 29-05-2025
+//Patern pring using for loop
+
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1 ; i<=4 ;i++)
+	{
+		for(j=1 ;j<=4 ; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+return 0;
+}
