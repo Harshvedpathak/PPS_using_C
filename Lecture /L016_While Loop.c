1c6f47438f2e15a5c6fec3bdf265c29528f2d211
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-06-05 10:40:35 +0530

    Update and rename L016_ to L016_While Loop.c

---
+++ Lecture /L016_While Loop.c
@@ -0,0 +1,15 @@
+//22-05-2025
+//Lecture_WAP to print Hello World 100 times using while Loop.
+
+#include<stdio.h>
+int main()
+{
+	int i=1;
+	while(i<=100)
+	{
+		printf("%d. Hello World\n",i);
+		i++;
+		
+	}
+return 0;
+}
