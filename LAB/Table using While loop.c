b93c740eaf75e9292bbe02428fe7249f2b1e99cf
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-06-12 11:17:17 +0530

    Create Table using While loop.c

---
+++ LAB/Table using While loop.c
@@ -0,0 +1,17 @@
+
+//WAP to print Table of any number using while loop
+
+#include <stdio.h>
+int main()
+{
+	int i=1,t,num;
+	printf("Enter number to print its Table :");
+	scanf("%d",num);
+	while(i<=10)
+		{
+			
+			printf("%d X %d = %d\n",i,num,);
+			i++;
+		}
+	return 0;
+}
