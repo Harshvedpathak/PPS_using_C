f4e363c76a30392e514c16a6b065b9c09e2a9ae3
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-07-05 17:03:06 +0530

    Create Print Number.c

---
+++ LAB/Practical File/Print Number.c
@@ -0,0 +1,14 @@
+// 1.2 To Accept a Number From User and Print it.
+// Harsh Vedpathak_8026
+
+#include <stdio.h>
+int main()
+{
+	
+  int a;
+	printf("Enter the Number to Print it : ");
+	scanf("%d", &a);
+	printf("The Entered Number is : %d",a);
+
+  return 0;
+}
