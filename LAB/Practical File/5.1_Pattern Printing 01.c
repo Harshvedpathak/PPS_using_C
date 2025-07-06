7394a0ce7498fd2ecaafd772da493f77ddfe251d
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-07-06 23:56:04 +0530

    Create 5.1_Pattern Printing 01.c

---
+++ LAB/Practical File/5.1_Pattern Printing 01.c
@@ -0,0 +1,22 @@
+// 5.1 Pattern Printing.
+
+//       *  *  *  *  
+//       *  *  *  *  
+//       *  *  *  *  
+//       *  *  *  * 
+// Harsh Vedpathak_8026.
+
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<=3;i++)
+    {
+        for(j=0;j<=3;j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
