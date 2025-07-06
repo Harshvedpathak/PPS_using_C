796c74043fe9c454085b6091cd7e2a862eecba4c
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-07-06 20:25:17 +0530

    Create 4.3_Table of 10 (Do While Loop).c

---
+++ LAB/Practical File/4.3_Table of 10 (Do While Loop).c
@@ -0,0 +1,15 @@
+// 4.3 Table of 10 Using Do While Loop.
+// Harsh Vedpathak_8026
+
+#include <stdio.h>
+int main()
+{
+  	int i=1;
+  	do
+	  {
+	  	printf("10 X %d = %d\n",i,i*10);
+		  i++;
+  	}
+  	while (i<=10);
+	  return 0;
+}
