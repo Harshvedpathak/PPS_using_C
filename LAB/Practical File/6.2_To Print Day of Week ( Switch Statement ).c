03c397bd565a33f293fe9e99e1d3350ffe2b62b1
Harsh Vedpathak <156324335+Harshvedpathak@users.noreply.github.com>
2025-07-07 21:10:50 +0530

    Create 6.2_To Print Day of Week ( Switch Statement ).c

---
+++ LAB/Practical File/6.2_To Print Day of Week ( Switch Statement ).c
@@ -0,0 +1,36 @@
+// 6.2 To Print Day of Week Using Switch Statement.
+// Harsh Vedpathak_8026.
+#include <stdio.h>
+int main()
+{
+    int day;
+    printf("Enter Day Number: ");
+    scanf("%d",&day);
+    switch(day)
+    {
+        case 1:
+        printf("Monday");
+        break;
+        case 2:
+        printf("Tuesday");
+        break;
+        case 3:
+        printf("Wednesday");
+        break;
+        case 4:
+        printf("Thursday");
+        break;
+        case 5:
+        printf("Friday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 7:
+        printf("Sunday");
+        break;
+        default:
+        printf("Invalid Input");
+    }
+    return 0;
+}
